1eba89633e12c99d3aa949fbfea8c0e7f2f72ebd
ElijahLenhard <elijahlenhard@gmail.com>
2023-11-19 16:56:10 -0500

    Adds basic camera movement

--- Scripts/Player/playerMove.gd
+++ Scripts/Player/playerMove.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 signal hit
 
-@export var top_speed = 200
+@export var top_speed = 250
 var screen_size
 var time_jumping =0
 var max_jump_time = .66
@@ -26,38 +26,47 @@ func _ready():
 func _physics_process(delta):
 	
 	apply_gravity(delta)
-	
+	#Checks for directional input and calls appropriate functions
 	if Input.is_action_pressed("move_right"):
 		run_right(delta)
-		dash_direction.x =1
+		dash_direction.x =1 #Dash direction updated for direction character is facing
 	elif Input.is_action_pressed("move_left"):
 		run_left(delta)
-		dash_direction.x =-1
+		dash_direction.x =-1#Dash direction updated for direction character is facing
 	else:
-		decelerate(delta, 1)
+		decelerate(delta, 1)#if no directional input, decelerate
 		
+	#Primes a jump the frame the jump key is pressed
+	if Input.is_action_just_pressed("jump"):
+		jump_primed =3#if the player touches the ground at anypoint in the next 3 frames, execute a jump
 		
-	if Input.is_action_pressed("jump"):
-		jump_primed = 3
-	else:
+	#sets jump_held to false if the jump key is released
+	if !Input.is_action_pressed("jump"):
 		jump_held = false
-	if jump_primed>0:
-		jump_primed -=1
+	
+	if jump_primed>0:#if a jump is primed
+		jump_primed -=1#decrement jump primed var
+		
+	#if I jump is primed and the player touches the floor call the jump function
+	#also checks if jump is currently held to handle holding jump key for higher jump.
 	if ((jump_primed>0 && is_on_floor()) || jump_held):
 		jump_held = true
 		jump(delta)
 	else:
 		time_jumping = 0
-		
+	
+	#if the dash key is hit and dash is off cool down call dash function
 	if(Input.is_action_just_pressed("dash") && remaining_dash_cd<=0):
-		is_dashing = true
+		is_dashing = true #disables deceleration and sets y velocity to 0 for a set time
 		dash(delta)
 		remaining_dash_cd = dash_cd
+		
 	if(is_dashing):
 		dash_time += delta
 		if (dash_time > .15):
 			is_dashing = false
 			dash_time =0
+		velocity.y =0
 			
 	if(remaining_dash_cd >0):
 		remaining_dash_cd -= delta
@@ -77,13 +86,10 @@ func _physics_process(delta):
 
 	print_debug(velocity)
 	
-	
-
 func apply_gravity(delta):
 	var linear = 1200
-	if !is_on_floor():
+	if (!is_on_floor()):
 		velocity.y += (linear*delta)*((terminal_velocity-velocity.y)/terminal_velocity)
-	
 
 func jump(delta):
 	var linear = 7000
@@ -97,7 +103,7 @@ func run_right(delta):
 	var acceleration = 1.1
 	var linear = 20
 	if(velocity.x<0||velocity.x>(top_speed+25)):
-		decelerate(delta, 1)
+		decelerate(delta, 2)
 	elif(velocity.x<top_speed):
 		velocity.x = velocity.x*acceleration + linear
 		
@@ -105,15 +111,16 @@ func run_left(delta):
 	var acceleration = 1.1
 	var linear = -20
 	if(velocity.x>0 ||velocity.x<-(top_speed+25)):
-		decelerate(delta, 1)
+		decelerate(delta, 2)
 	elif(velocity.x>-top_speed):
 		velocity.x = velocity.x*acceleration + linear
 
 func decelerate(delta, rate):
 	if(is_dashing):
-		print_debug("isDashing")
 		return
-	var deceleration = .9
+	var deceleration = .95
+	if(rate == 2):
+		deceleration = .9
 	var linear = 20
 	if(abs(velocity.x) < linear):
 		velocity.x = 0
@@ -126,11 +133,3 @@ func dash(delta):
 	velocity.x = 1000*dash_direction.x
 		
 	
-
-
-"""func _on_body_entered(body):
-	hide()
-	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
-	"""
