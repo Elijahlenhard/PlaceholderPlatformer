b63391ef31f02f696bce337ea51c2d73bcdb4f57
ElijahLenhard <elijahlenhard@gmail.com>
2023-11-19 16:03:16 -0500

    Added basic dashing functionality

--- Scripts/Player/playerMove.gd
+++ Scripts/Player/playerMove.gd
@@ -10,6 +10,11 @@ var max_jump_time = .66
 @export var g = 1
 var jump_primed = -1
 var jump_held
+var dash_direction = Vector2(0,1)
+var dash_time = 0
+var is_dashing = false
+var dash_cd = 1.5
+var remaining_dash_cd = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,15 +28,16 @@ func _physics_process(delta):
 	apply_gravity(delta)
 	
 	if Input.is_action_pressed("move_right"):
-		accelerate_right(delta)
-
+		run_right(delta)
+		dash_direction.x =1
 	elif Input.is_action_pressed("move_left"):
-		accelerate_left(delta)
+		run_left(delta)
+		dash_direction.x =-1
 	else:
 		decelerate(delta, 1)
 		
 		
-	if Input.is_action_pressed("Jump"):
+	if Input.is_action_pressed("jump"):
 		jump_primed = 3
 	else:
 		jump_held = false
@@ -42,6 +48,19 @@ func _physics_process(delta):
 		jump(delta)
 	else:
 		time_jumping = 0
+		
+	if(Input.is_action_just_pressed("dash") && remaining_dash_cd<=0):
+		is_dashing = true
+		dash(delta)
+		remaining_dash_cd = dash_cd
+	if(is_dashing):
+		dash_time += delta
+		if (dash_time > .15):
+			is_dashing = false
+			dash_time =0
+			
+	if(remaining_dash_cd >0):
+		remaining_dash_cd -= delta
 
 	if velocity.length() > 0:
 		$AnimatedSprite2D.play()
@@ -56,8 +75,8 @@ func _physics_process(delta):
 		# See the note below about boolean assignment.
 		$AnimatedSprite2D.flip_h = velocity.x < 0
 
-		
 	print_debug(velocity)
+	
 	
 
 func apply_gravity(delta):
@@ -74,23 +93,26 @@ func jump(delta):
 		velocity.y -= linear*((pow(max_jump_time, .1)-pow(time_jumping,.1))/max_jump_time)*delta
 		time_jumping += delta
 
-func accelerate_right(delta):
+func run_right(delta):
 	var acceleration = 1.1
 	var linear = 20
-	if(velocity.x<0):
+	if(velocity.x<0||velocity.x>(top_speed+25)):
 		decelerate(delta, 1)
 	elif(velocity.x<top_speed):
 		velocity.x = velocity.x*acceleration + linear
 		
-func accelerate_left(delta):
+func run_left(delta):
 	var acceleration = 1.1
 	var linear = -20
-	if(velocity.x>0):
+	if(velocity.x>0 ||velocity.x<-(top_speed+25)):
 		decelerate(delta, 1)
 	elif(velocity.x>-top_speed):
 		velocity.x = velocity.x*acceleration + linear
 
 func decelerate(delta, rate):
+	if(is_dashing):
+		print_debug("isDashing")
+		return
 	var deceleration = .9
 	var linear = 20
 	if(abs(velocity.x) < linear):
@@ -100,7 +122,9 @@ func decelerate(delta, rate):
 	elif(velocity.x < 0):
 		velocity.x = velocity.x*deceleration + linear
 
-	
+func dash(delta):
+	velocity.x = 1000*dash_direction.x
+		
 	
 
 
