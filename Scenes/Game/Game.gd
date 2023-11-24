cb21c25893d902d136d277a160b30552f333d189
ElijahLenhard <elijahlenhard@gmail.com>
2023-11-24 18:17:50 -0500

    Changed layout of world

---
+++ Scenes/Game/Game.gd
@@ -0,0 +1,18 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_level_a_change_level(level):
+	var new_level = load("res://Scenes/Worlds/" + level +".tscn")
+	var level_instance = new_level.instantiate()
+	level_instance.set_name(level)
+	add_child(level_instance)
