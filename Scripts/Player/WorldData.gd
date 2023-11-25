99efcfb3d3fea0b48171b2e123bce168d00eab05
Garrett Chase <gcofficial19@gmail.com>
2023-11-24 19:58:13 -0500

    Saving and loading player data works (only save and load functionality, not actual player data yet). Also quit button in escape menu works.

--- Scripts/Player/WorldData.gd
+++ Scripts/Player/WorldData.gd
@@ -2,9 +2,12 @@ extends Resource
 
 class_name WorldData
 
-@export var player_name: String = ""
-@export var level: int = 1
+@export var player_name: String
+@export var level: int
 
 @export var has_dash: bool
 @export var has_double_jump: bool
 @export var has_wall_grab: bool
+
+func _to_string():
+	return player_name + ", " + str(level) + ", " + str(has_dash) + ", " + str(has_double_jump) + ", " + str(has_wall_grab)
--- Scripts/Saves/SaveManager.gd
+++ Scripts/Saves/SaveManager.gd
@@ -1,4 +1,10 @@
+class_name SaveManager
+
 extends Node
+
+var save_path: String = "user://saves/"
+
+@export var world_state: WorldState
 
 @export var has_dash: bool:
 	get = get_has_dash
@@ -12,14 +18,17 @@ extends Node
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	verify_save_directory(save_path)
+
+func verify_save_directory(path: String):
+	DirAccess.make_dir_absolute(path)
 
 func save_game(world_data):
-	var date = Time.get_date_dict_from_system();
-	var time = Time.get_time_dict_from_system();
-	var FILE_NAME = "PlayerSave-%s_%s_%s_%s_%s_%s" % [date["year"], date["month"], date["day"], 
+	var date = Time.get_date_dict_from_system()
+	var time = Time.get_time_dict_from_system()
+	var FILE_NAME = "PlayerSave-%s_%s_%s_%s_%s_%s.tres" % [date["year"], date["month"], date["day"], 
 		time["hour"], time["minute"], time["second"]]
-	var result = ResourceSaver.save(FILE_NAME, world_data)
+	var result = ResourceSaver.save(world_data, save_path + FILE_NAME)
 	assert(result == OK)
 
 func load_game(FILE_NAME):
@@ -36,3 +45,22 @@ func get_has_double_jump():
 
 func get_has_wall_grab():
 	return has_wall_grab
+
+
+func _on_escape_menu_save_pressed():
+	save_game(world_state.state)
+
+
+func _on_escape_menu_load_pressed():
+	var dir = DirAccess.get_files_at(save_path)
+	var recent = dir[0]
+	for file_name in dir:
+		if FileAccess.get_modified_time(file_name) > FileAccess.get_modified_time(recent):
+			recent = file_name
+		
+	var data = load_game(save_path + recent)
+	print(data._to_string())
+
+
+func _on_escape_menu_exit_pressed():
+	get_tree().quit()
